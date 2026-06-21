7344f32e6dc1db4f755866bdfcb846ad2f1d3ba6
Khisamutdinov Kirill <kirillhisa.work@gmail.com>
2026-06-21 23:58:04 +0300

    Remove duplicating of allowed hosts in production config

--- mcl_site/settings/production.py
+++ mcl_site/settings/production.py
@@ -18,11 +18,7 @@ SECRET_KEY = os.environ.get(
     "SECRET_KEY",
     "temporary-build-secret"
 )
-# --------------------------------------------------
-# HOSTS
-# --------------------------------------------------
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 # --------------------------------------------------
 # CSRF / HTTPS
 # --------------------------------------------------
