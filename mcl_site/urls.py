f2a547f86e6763549c4b098056202a87137a993b
Khisamutdinov Kirill <kirillhisa.work@gmail.com>
2026-07-12 19:45:54 +0300

    serve media files in production, not only in DEBUG mode

--- mcl_site/urls.py
+++ mcl_site/urls.py
@@ -3,6 +3,8 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 from django.contrib import admin
 from django.http import HttpResponse
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -26,27 +28,22 @@ urlpatterns = [
     path(
         "robots.txt",
         lambda request: HttpResponse(
-            "User-agent: *\nDisallow: /admin/\nDisallow: /django-admin/\nSitemap: https://velychko.pythonanywhere.com/sitemap.xml\n",
+            f"User-agent: *\nDisallow: /admin/\nDisallow: /django-admin/\nSitemap: https://{request.get_host()}/sitemap.xml\n",
             content_type="text/plain",
         ),
         name="robots_txt",
     ),
 ]
 
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
     # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    try:
-        import debug_toolbar  # noqa: F401
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+try:
+    import debug_toolbar  # noqa: F401
 
-        urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
-    except ImportError:
-        pass
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+except ImportError:
+    pass
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
