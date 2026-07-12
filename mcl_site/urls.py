db459ef756776c516a9172c546031fb827e77c84
Khisamutdinov Kirill <kirillhisa.work@gmail.com>
2026-07-12 20:22:36 +0300

    use django.views.static.serve directly for media, bypassing DEBUG check baked into static()

--- mcl_site/urls.py
+++ mcl_site/urls.py
@@ -3,8 +3,8 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 from django.contrib import admin
 from django.http import HttpResponse
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import re_path
+from django.views.static import serve
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -35,22 +35,16 @@ urlpatterns = [
     ),
 ]
 
-    # Serve static and media files from development server
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-try:
-    import debug_toolbar  # noqa: F401
-
-    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
-except ImportError:
-    pass
-
-urlpatterns = urlpatterns + [
-    # For anything not caught by a more specific rule above, hand over to
-    # Wagtail's page serving mechanism. This should be the last pattern in
-    # the list:
-    path("", include(wagtail_urls)),
-    # Alternatively, if you want Wagtail pages to be served from a subpath
-    # of your site, rather than the site root:
-    #    path("pages/", include(wagtail_urls)),
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
+
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+
+    try:
+        import debug_toolbar  # noqa: F401
+        urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+    except ImportError:
+        pass
